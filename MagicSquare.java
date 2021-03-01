12a9b52931ccfd72aa3921a0b91b34c96df5c459
Nick B <69913944+n-brunetti@users.noreply.github.com>
2021-03-01 12:18:03 -0600

    Update MagicSquare.java

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,7 +1,7 @@
 
 /**
- * @author 
- * @version 
+ * @author Nick Brunetti
+ * @version  March 1, 2021
  */
 
 public class MagicSquare
@@ -25,7 +25,66 @@ public class MagicSquare
      */
     public boolean isMagicSquare()
     {
-        return false;   // complete this method
+      
+      if(array[0].length!=array.length){
+        return false;
+      }
+int base =0;
+
+ for(int w = 0; w<array[0].length; w++){
+          base += array[0][w];
+        }
+      
+      int diagonal=0, column=0, rows =0, diagonal2 = 0;
+      boolean diagon=true, colum=true, row=true, diagon2 = true;
+
+/** row check
+*/
+      for(int i = 0; i<array.length; i++){
+        for(int j = 0; j<array[i].length; j++){
+          rows += array[i][j];
+        }
+        if(rows !=base){
+          row = false;
+          }
+          rows = 0;
+      }    
+     
+/** column check
+*/
+      for(int i = 0; i<array[0].length; i++){
+        for(int j = 0; j<array.length; j++){
+          column +=array[j][i]; 
+        }
+        if(column !=base){
+          colum = false;
+          }
+          column =0;
+      }   
+
+/** diagonal top left to bottom right check
+*/
+      for(int i = 0; i<array[0].length; i++){
+           diagonal +=array[i][i];
+      }
+        if(diagonal !=base){
+           diagon = false;
+        }
+
+/** diagonal  bottom left to top right check
+*/
+      for(int i = array[0].length-1; i>=0; i--){
+           diagonal2 +=array[array[0].length-1-i][i];
+      }
+        if(diagonal2 !=base){
+           diagon2 = false;
+        }
+
+if(row == true && colum == true && diagon == true && diagon2 == true){
+  return true;
+}else{
+  return false;
+}
     }
 
     /**
